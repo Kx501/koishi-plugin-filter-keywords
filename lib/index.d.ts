8a336c30208517f0ee74b4bda71dd1e49439255c
Kxy051 <a2483654847@gmail.com>
2024-03-15 19:56:46 +0800

    modified:   lib/index.d.ts 	modified:   lib/index.js 	modified:   package.json 	modified:   src/index.ts

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,13 +1,15 @@
 import { Context, Schema } from 'koishi';
 export declare const name = "filter-keywords";
+export declare const usage = "\u76EE\u524D\u53EA\u652F\u6301\u53C2\u6570\u5728\u672B\u5C3E\u7684\u60C5\u51B5\uFF0C\u6709\u7A7A\u518D\u6539\u3002";
 export interface Config {
+    关键词: string;
     过滤关键词: boolean;
     替换关键词: boolean;
-    不响应消息: boolean;
+    自定义替换文本: string;
     撤回消息: boolean;
-    关键词: string;
-    测试回复: boolean;
-    自定义分隔符: string;
+    触发提示: boolean;
+    自定义提示文本: string;
+    回复调试信息: boolean;
 }
 export declare const Config: Schema<Config>;
 export declare function apply(ctx: Context, config: Config): void;
--- src/index.ts
+++ src/index.ts
@@ -1,113 +1,125 @@
-import { Context, Schema, h, Dict, Session, Logger } from 'koishi'
+import { Context, Schema, h, Dict, Session, Logger, Argv } from 'koishi'
 import Mint from 'mint-filter'
 
 
 export const name = 'filter-keywords'
 
+export const usage = '目前只支持参数在末尾的情况，有空再改。'
+
 export interface Config {
+  关键词: string,
   过滤关键词: boolean,
   替换关键词: boolean,
-  // 自定义替换文本: string,
-  不响应消息: boolean,
+  自定义替换文本: string,
+  // 不响应: boolean,
   撤回消息: boolean,
-  关键词: string,
-  测试回复: boolean,
-  自定义分隔符: string,
+  触发提示: boolean,
+  自定义提示文本: string,
+  // 排除的指令: any,
+  // 多位置支持: boolean,
+  // 参数位置: any,
+  回复调试信息: boolean,
 }
 
 export const Config: Schema<Config> = Schema.object({
+  关键词: Schema.string().role('textarea', { rows: [3, 100] }).description('用中/英文逗号隔开。'),
   过滤关键词: Schema.boolean().default(true).description('从消息中删除关键词。'),
   替换关键词: Schema.boolean().default(false).description('将关键词替换为“*”。'),
-  // 自定义替换文本: Schema.string().default('*').description('支持字符串。'),
-  不响应消息: Schema.boolean().default(false).description('触发关键词没有任何响应。'),
-  撤回消息: Schema.boolean().default(true).description('需要管理员权限。'),
-  关键词: Schema.string().role('textarea', { rows: [3, 100] }).description('用中/英文逗号分割。'),
-  测试回复: Schema.boolean().default(true).description('机器人会重复过滤后的文本。'),
-  自定义分隔符: Schema.string().default(', ').description('自定义处理后文本的分隔符。'),
+  自定义替换文本: Schema.string().default('*').description('只支持字符串。'),
+  // 不响应: Schema.boolean().default(false).description('触发关键词不响应消息。'),
+  撤回消息: Schema.boolean().default(false).description('需要管理员权限。'),
+  触发提示: Schema.boolean().default(false).description('触发后不执行指令，并且提示。'),
+  自定义提示文本: Schema.string().default('触发敏感词了哦~').description('自定义提示消息。'),
+  // 多位置支持: Schema.boolean().default(false).description('支持传入指令的参数有多个空格隔开的情况。'),
+  // 参数位置: Schema.boolean().default(false).description('触发后提示。'),
+  回复调试信息: Schema.boolean().default(false).description('机器人会回复触发的关键词，以及处理后的文本。'),
 })
 
 export function apply(ctx: Context, config: Config) {
   const logger = new Logger('filter-keywords')
+  logger.debug('开启调试模式。')
 
+
+  // 格式化关键词数组
+  const keywords = processKeywords(config.关键词);
 
   function processKeywords(inputString: string) {
-    // 按逗号分割
-    const keywordsArray = inputString.split(/[,，]/);
-    // 去除最外层空格
-    const processedKeywords = keywordsArray.map(keyword => keyword.trim());
-    return processedKeywords;
+    if (inputString.length > 0) {
+      // 按逗号分割
+      const keywordsArray = inputString.split(/[,，]/);
+      // 去除最外层空格
+      const processedKeywords = keywordsArray.map(keyword => keyword.trim());
+      return processedKeywords;
+    } else {
+      logger.error('过滤前确保填入关键词！')
+    }
   }
 
-// 过滤关键词
-function filterKeywords(text: string, keywords: string[]) {
-  const mint = new Mint(keywords);
-  return mint.filter(text).text;
-}
+  // 过滤关键词
+  function filterKeywords(text: string, keywords: string[]) {
+    const mint = new Mint(keywords, { customCharacter: '' });
+    // logger.debug(mint)
+    return mint.filter(text, { replace: true });
+  }
 
-// 替换关键词
-function replaceKeywords(text: string, keywords: string[]) {
-  const mint = new Mint(keywords);
-  return mint.filter(text, { replace: true }).text;//.replace(/\*+/g, replaceChar);
-}
+  // 替换关键词
+  function replaceKeywords(text: string, keywords: string[]) {
+    const mint = new Mint(keywords, { customCharacter: `${config.自定义替换文本}` });
+    return mint.filter(text, { replace: true });//.replace(/\*+/g, replaceChar);
+  }
 
-type Fragment = Element[]
-type Transformer = ((
-  attrs: Dict,
-  children: Element[],
-  session: Session,
-) => Fragment)
-
-
-
-  const keywordsArray = processKeywords(config.关键词);
-  ctx.on('before-parse', (content, session) => {
-    let result: any;
-    let rules: Transformer;
-    const text = session.content;
-    logger.info(ctx)
-
-    if (config.过滤关键词) {
-      const result = filterKeywords(text, keywordsArray)
-    } else if (config.替换关键词) {
-      // h.transform(replaceKeywords(text, keywordsArray), rules){
-      // };
-    } else if (config.撤回消息) {
-      
-    } else if (config.不响应消息) {
-
+  // 提示信息&撤回
+  function prompt(result: any, session: Session<never, never, Context>, content: string) {
+    const triggerFlag = result.words.length
+    if (config.触发提示 && triggerFlag > 0) {
+      logger.debug('不执行......')
+      session.send(h('at', { id: session.userId }) + config.自定义提示文本)
+      content = ''
+    } else {
+      content = result.text
     }
 
-    // 敏感词数组
-    const mint = new Mint(['敏感词数组'])
+    if (config.撤回消息 && triggerFlag > 0) {
+      session.bot.deleteMessage(session.channelId, session.messageId)
+    }
+    return content
+  }
 
-    // 基本使用
-    mint.filter('需要验证的文本')
-    return result
+
+  ctx.on('before-parse', (content, session,) => {
+    // const elements = h.select(content, 'text')
+    // logger.debug('原始消息: ', elements)
 
 
+    logger.debug('处理前: ', content)
+
+
+    if (config.过滤关键词) {
+      logger.debug('过滤......')
+      const result = filterKeywords(content, keywords)
+      content = prompt(result, session, content)
+
+    } else if (config.替换关键词) {
+      logger.debug('替换......')
+      const result = replaceKeywords(content, keywords)
+      content = prompt(result, session, content)
+    }
+
+    // if (quote?.content) {
+    //   argv.tokens.push({
+    //     content: quote.content,
+    //     quoted: true,
+    //     inters: [],
+    //     terminator: '',
+    //   })
+    // }
+
+
+
+    // 解析消息
+    // const { quote, isDirect, stripped: { prefix, appel } } = session
+    const argv = Argv.parse(content)
+    logger.debug('处理后: ', argv)
+    return argv;
   }, true)
-
-  // ctx.middleware(
-  //   async (session, next) => {
-  //     const text = session.content;
-
-  //     if (config.过滤关键词) {
-  //       filterKeywords(text, keywordsArray)
-  //     } else if (config.替换关键词) {
-  //       replaceKeywords(text, keywordsArray)
-  //     } else if (config.撤回消息) {
-        
-  //     } else if (config.不响应消息) {
-
-  //     } return next();
-
-  //     // 敏感词数组
-  //     const mint = new Mint(['敏感词数组'])
-
-  //     // 基本使用
-  //     mint.filter('需要验证的文本')
-
-
-  //   }, true)
-
 }
